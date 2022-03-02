a0a696acf67b7096f0276d61e25dfb3ab08463f8
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-02 15:16:58 -0500

    temp seed file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,16 @@
-// const sequelize = require('../config/connection');
-// // const { User } = require('../models');
+const sequelize = require('../config/connection');
+const { Recipe } = require('../models');
 
-// // const userData = require('./userData.json');
+const recipeData = require('./recipeData.json');
 
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
+const seedDatabase = async() => {
+  await sequelize.sync({ force: true });
+  await Recipe.bulkCreate(recipeData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-//   await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
+  process.exit(0);
+};
 
-//   process.exit(0);
-// };
-
-// seedDatabase();
+seedDatabase();
