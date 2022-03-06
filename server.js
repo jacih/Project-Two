57ecb3ebe5243ea32efcb0e9918edf9f4bd330d5
Julian Quan Fun <julianquanfun@gmail.com>
2022-03-06 17:24:43 -0500

    handlebars update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 const recipeScraper = require('recipe-scraper');
 
 const sequelize = require('./config/connection');
@@ -12,7 +12,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ //helpers 
+});
 
 const sess = {
   secret: 'Super secret secret',
