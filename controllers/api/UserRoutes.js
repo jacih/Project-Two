d653ea2384dc1bffcfa7c96ca67f6e6e9c6ce227
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-07 22:29:02 -0500

    minor refactor for final bug fix attempts

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -1,69 +1,70 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-
-// Create User
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create({
-        username: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
+const bcrypt = require('bcrypt');
+const { User } = require('../../models/User');
 
 
-        req.session.save(() => {
-            req.session.logged_in = true;
-
-            res.status(200).json(userData);
-        })
-    } catch (err) {
-        res.status(400).json(err);
-    }
-})
-
-//User Login
-router.post('/login', async (req, res) => {
+//get all users;
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'Login Successful' });
-    });
-
+    const users = await User.findAll();
+    res.send(users);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).send(err);
+    console.log(err);
   }
 });
 
-//User logout
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-})
+// Create User;
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      favoriteFood: req.body.favoriteFood,
+    });
+    res.status(201).send(newUser);
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+});
 
+//User Login;
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ 
+      where: { 
+        email: req.body.email 
+      } 
+    });
+    const user = data.get({ plain: true });
+
+    if (user) {
+      const validPassword = await bcrypt.compare(req.body.password, user.password);
+      // console.log('valid password: ', validPassword);
+      if (validPassword) {
+        req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.status(201).send('Login Successful');
+      })
+      } else {
+        res.json({ message: 'Incorrect password' });
+      }
+    } else {
+      res.json({ message: 'Incorrect email' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+//log out user, end session;
+router.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.json({ message: 'user has logged out' });
+  console.log(req.session);
+});
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,79 +1,62 @@
 const router = require('express').Router();
-const { User, Meal } = require('../models');
+const { User, Meal, Recipe } = require('../models');
 const withAuth = require ('../utils/auth');
 
-// route to render home page
+//Route to get all meals and JOINs with user data and recipe data;
 router.get('/', async (req, res) => {
-    res.render('homepage');
-});
+  try {
+    const mealData = await Meal.findAll({
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Recipe,
+        },
+      ],
+    });
+    //serialize data so it can be read;
+    const meals = mealData.map((meal) => meal.get({ plain: true })
+    );
 
-//Route to get all meals
-// router.get('/', async (req, res) => {
-//     const allmealData = await Meal.findAll().catch((err) => {
-//       res.json(err);
-//     })
-  
-//     const meals = allmealData.map((meal) => meal.get({ plain: true }));
-//     res.render ('getmeals', {meals});
-// });
-
-//Route to get a single meal
-router.get('/meal/:id', async (req, res) => {
-    try {
-        const MealPlan = await Meal.findbyPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                }
-            ]
-        })
-        
-        const meals = MealPlan.get({ plain: true });
-
-        res.render('meal', {
-            ...meals,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-
-router.get('/mealplan', withAuth, async (req, res) => {
-    try {
-        const userData = await User.findbyPk(req.session.user_id, {
-            attributes: { excludes: ['password'] },
-            include: [{ model: Meal }],
-        });
-
-        const user = userData.get({ plain: true });
-
-        res.render('mealplan', {
-            ...user,
-            logged_in: true
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-
-router.get('/register', (req, res) => {
+    // set up user session
     if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
-    res.render('register');
-})
+      const userData = await User.findByPk(req.session.user_id);
+      const user = userData.get({ plain: true });
 
+      res.render('homepage', {
+        meals,
+        user,
+        logged_in: req.session.logged_in,
+      });
+    } else {
+      res.render('homepage', {
+        meals,
+        logged_in: req.session.logged_in,
+      });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});  
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
-})
+  if (req.session.logged_in) {
+    window.alert('You are currently logged in');
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+  
+router.get('/register', (req, res) => {
+  if (req.session.logged_in) {
+    window.alert('You are currently logged in');
+    res.redirect('/');
+    return;
+  }
+  res.render('register');
+});
+
 
 module.exports = router;
